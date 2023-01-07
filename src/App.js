ffdddc423601bc3cda10c593b288a58446a21b5a
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-01-07 22:38:21 +0330

    add search bar and table to layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import { Container } from "@mui/material";
+import { Container, Grid, Button } from "@mui/material";
+import Item from "@mui/material/Grid";
 import SearchBar from "./components/SearchBar";
+import ProductTable from "./components/ProductTable";
 
 function App() {
   return (
     <Container>
-      <SearchBar />
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Item>
+            <SearchBar />
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+            <Button variant="contained" color="success">
+              Add
+            </Button>
+            <Button variant="contained" color="secondary">
+              Refresh
+            </Button>
+          </Item>
+        </Grid>
+        <Grid item xs={12}>
+          <Item>
+            <ProductTable />
+          </Item>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
