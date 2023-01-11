4df2e09bb0b6101f2a865e07831cfd85195dad5e
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-01-11 10:43:24 +0330

    add form validation

--- src/components/CreateModal.js
+++ src/components/CreateModal.js
@@ -6,8 +6,9 @@ import { useState } from "react";
 
 const CreateModal = ({ open, handleClose }) => {
   const [name, setName] = useState("");
-  const [price, setPrice] = useState(undefined);
-  const [count, setCount] = useState(undefined);
+  const [price, setPrice] = useState("");
+  const [count, setCount] = useState("");
+  const [errors, setErrors] = useState({ name: "", price: "", count: "" });
 
   const createNewProduct = async () => {
     const response = await axios.post(PRODUCTS_URL, {
@@ -17,6 +18,29 @@ const CreateModal = ({ open, handleClose }) => {
     });
     handleClose();
   };
+
+  const validateName = (e) => {
+    const { value } = e.target;
+    setErrors({ ...errors, name: "" });
+    if (!value) {
+      setErrors({ ...errors, name: "Name can not be empty" });
+    } else if (new RegExp("[0-9]").test(value)) {
+      setErrors({ ...errors, name: "Name can not contain numbers" });
+    }
+    setName(value);
+  };
+
+  const validateCount = (e) => {
+    const { value } = e.target;
+    setErrors({ ...errors, count: "" });
+    if (!value) {
+      setErrors({ ...errors, count: "Count can not be empty" });
+    } else if (Number(value) > 1000) {
+      setErrors({ ...errors, count: "Count can not be more than 1000" });
+    }
+    setCount(value);
+  };
+
   return (
     <Modal
       open={open}
@@ -33,14 +57,15 @@ const CreateModal = ({ open, handleClose }) => {
         fullWidth
         variant="standard"
         value={name}
-        onChange={(e) => setName(e.target.value)}
+        onChange={validateName}
       />
+      {errors.name && <span style={{ color: "red" }}>{errors.name}</span>}
       <TextField
         autoFocus
         margin="dense"
         id="price"
         label="Price"
-        type="number"
+        type="text"
         fullWidth
         variant="standard"
         value={price}
@@ -55,8 +80,9 @@ const CreateModal = ({ open, handleClose }) => {
         fullWidth
         variant="standard"
         value={count}
-        onChange={(e) => setCount(e.target.value)}
+        onChange={validateCount}
       />
+      {errors.count && <span style={{ color: "red" }}>{errors.count}</span>}
     </Modal>
   );
 };
